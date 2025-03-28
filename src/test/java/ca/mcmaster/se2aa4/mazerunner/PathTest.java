2b27ae40a1d5d58852c3f41343f50f030df55e1f
udaysinghkanwar <udaykanwar007@gmail.com>
2025-03-28 15:03:59 -0400

    created initial test directory and file

---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/PathTest.java
@@ -0,0 +1,18 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.junit.jupiter.api.Test;
+
+class PathTest {
+
+
+    
+    @Test
+    void testMazeFunctionality() {
+
+
+        // Add a meaningful test case based on Maze's functionality
+        assert 1 == 2; // Placeholder assertion, replace with actual test logic
+    }
+
+
+}
